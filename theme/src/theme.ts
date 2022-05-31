f6bd1edeba1c41a8cdddd1437776e04a735b8de5
Muneeskumar Rajendran <muneeskumar.rajendran@bounteous.com>
2022-05-31 20:24:12 +0530

    less testing

--- theme/src/theme.ts
+++ theme/src/theme.ts
@@ -1,9 +1,4 @@
 
 // Stylesheets
-import "./theme.scss";
-import "./resources/images/favicon.png"
+import "./theme.less";
 
-// Component Javascript
-import "./components/experiencefragment/change_flag.js";
-import "./components/form-options/form_dropdown.js";
-import "./components/progressbar/progressbar.js";
